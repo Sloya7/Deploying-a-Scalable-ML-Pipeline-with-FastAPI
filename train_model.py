f5c93eaf5dad74bd34774c52bc49be91b76cd513
sloya7 <sloya7@wgu.edu>
2024-07-30 11:19:08 -0600

    train_model script developed

--- train_model.py
+++ train_model.py
@@ -13,14 +13,14 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "
+project_path = "/home/sloya7/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data)
+train, test = train_test_split(data, random_state = 42)
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,6 +36,10 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
+    X = train, 
+    categorical_features = cat_features,
+    training = True
+    
     # your code here
     # use the train dataset 
     # use training=True
@@ -52,7 +56,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = # your code here
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -66,7 +70,7 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = # your code here
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -79,6 +83,10 @@ for col in cat_features:
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
+            data = test,
+            column_name = col,
+            slice_value = slicevalue, 
+            model = model
             # your code here
             # use test, col and slicevalue as part of the input
         )
