a3544b50fa4f786d615ea9d9444c943646328b9e
Sloya7 <167228516+Sloya7@users.noreply.github.com>
2024-08-13 13:51:34 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "/home/sloya7/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = os.path.dirname(__file__)
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
